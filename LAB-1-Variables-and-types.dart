628fe203871d197d156c24155106e461e7e27478
Amantsehay <amanueltsehay11@gmail.com>
2024-03-05 16:44:40 +0300

    exercise

---
+++ LAB-1-Variables-and-types.dart
@@ -0,0 +1,9 @@
+void main() {
+  int a = 10;
+  int b = 20;
+  int c = 30;
+
+  int sumOfVariables = a + b + c;
+
+  print("Sum of a, b, and c is: $sumOfVariables");
+}
---
+++ LAB-2-Variables-and-types.dart
@@ -0,0 +1,5 @@
+void main() {
+  String strVariable = "Hello, World!";
+
+  print("Length of the string is: ${strVariable.length}");
+}
